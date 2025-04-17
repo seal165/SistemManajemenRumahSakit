afa90239f5ee139d031f1ce234edd24b1e73fde2
Eolia Shalbillah Gadis Suwandi <eoliasuwandi@gmail.com>
2025-04-17 23:21:37 +0700

    Update Dokter.java

--- Tugas 1/src/RumahSakit/Dokter.java
+++ Tugas 1/src/RumahSakit/Dokter.java
@@ -2,27 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package RumahSakit;
+package Bab1_ManajemenRumahSakit;
 
 /**
  *
  * @author Eolia Shalbillah
  */
 public class Dokter {
-    private String nama;
-    private String spesialis;
+    String dokter;
 
-    public Dokter(String nama, String spesialis) {
-        this.nama = nama;
-        this.spesialis = spesialis;
+    void dataDokter(String Dokter){
+        this.dokter = Dokter;
     }
-
-    public String getNama() { return nama; }
-    public String getSpesialis() { return spesialis; }
-
-    // Override toString() supaya dropdown dokter lebih rapi
-    @Override
-    public String toString() {
-        return nama + " - " + spesialis;
+    
+    String cetakDokter(){
+        return dokter;
     }
 }
