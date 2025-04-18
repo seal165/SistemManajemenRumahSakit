efe75287ec53fa2c2132107642e7e54b87ac27c8
Eolia Shalbillah Gadis Suwandi <eoliasuwandi@gmail.com>
2025-04-18 14:39:15 +0700

    Update Dokter.java

--- Tugas 2/src/ManajemenRumahSakit/Dokter.java
+++ Tugas 2/src/ManajemenRumahSakit/Dokter.java
@@ -2,31 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ManajemenRumahSakit;
+package Bab2_ManajemenRumahSakit;
 
 /**
  *
  * @author Eolia Shalbillah
  */
-public class Dokter { // Mendeklarasikan kelas Dokter
-    private String nama; // Variabel untuk menyimpan nama dokter
-    private String spesialis; // Variabel untuk menyimpan spesialisasi dokter
+public class Dokter {
+    String dokter;
 
-    // Konstruktor untuk menginisialisasi objek Dokter dengan nama dan spesialisasi
-    public Dokter(String nama, String spesialis) {
-        this.nama = nama; // Menetapkan nilai parameter nama ke variabel nama
-        this.spesialis = spesialis; // Menetapkan nilai parameter spesialis ke variabel spesialis
+    public Dokter(String dokter) {
+        this.dokter = dokter;
     }
 
-    // Method getter untuk mendapatkan nama dokter
-    public String getNama() { return nama; }
-    
-    // Method getter untuk mendapatkan spesialisasi dokter
-    public String getSpesialis() { return spesialis; }
+    void dataDokter(String Dokter){
+        this.dokter = Dokter;
+    }
 
-    // Override method toString untuk mengembalikan representasi string dari objek Dokter
-    @Override
-    public String toString() {
-        return nama + " - " + spesialis; // Menggabungkan nama dan spesialisasi dengan tanda " - "
+    String cetakDokter(){
+        return dokter;
     }
 }
