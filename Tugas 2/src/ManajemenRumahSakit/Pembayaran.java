6fa94f514d05aa21736f522ea6787a13f10e3b31
Eolia Shalbillah Gadis Suwandi <eoliasuwandi@gmail.com>
2025-04-18 14:40:51 +0700

    Update Pembayaran.java

--- Tugas 2/src/ManajemenRumahSakit/Pembayaran.java
+++ Tugas 2/src/ManajemenRumahSakit/Pembayaran.java
@@ -2,26 +2,50 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ManajemenRumahSakit;
+package Bab2_ManajemenRumahSakit;
 
 /**
  *
  * @author Eolia Shalbillah
  */
-import javax.swing.JOptionPane;
-import javax.swing.JOptionPane;
-import javax.swing.JOptionPane; // Mengimpor JOptionPane untuk menampilkan dialog konfirmasi pembayaran
+public class Pembayaran {
+    double jumlah;
+    String metode;
+    String namaPasien;
+    String status = "Belum Lunas";
 
-public class Pembayaran { // Mendeklarasikan kelas Pembayaran
-    
-    // Method untuk menampilkan dialog konfirmasi pembayaran dan mengembalikan hasilnya
-    public static boolean konfirmasiBayar(Pasien pasien, MetodePembayaran metode) {
-        int confirm = JOptionPane.showConfirmDialog(null, // Menampilkan dialog konfirmasi
-                "Nama Pasien: " + pasien.getNama() + // Menampilkan nama pasien di dialog
-                "\nTotal Bayar: Rp 500.000\nMetode: " + metode, // Menampilkan total pembayaran dan metode
-                "Konfirmasi Pembayaran", // Judul dialog konfirmasi
-                JOptionPane.YES_NO_OPTION); // Menyediakan opsi "Yes" dan "No"
-        
-        return confirm == JOptionPane.YES_OPTION; // Mengembalikan true jika pengguna memilih "Yes", sebaliknya false
+    public Pembayaran(double jumlah, String metode, String namaPasien) {
+        this.jumlah = jumlah;
+        this.metode = metode;
+        this.namaPasien = namaPasien;
+    }
+
+    void dataPembayaran(double jumlah, String metode, String namaPasien) {
+        this.jumlah = jumlah;
+        this.metode = metode;
+        this.namaPasien = namaPasien;
+    }
+
+    String cetakPembayaran() {
+        return String.valueOf(jumlah);
+    }
+
+    String cetakMetode() {
+        return metode;
+    }
+
+    String cetakStatus() {
+        return status;
+    }
+
+    void setStatus(String status) {
+        this.status = status;
+    }
+
+    boolean konfirmasiBayar(Pasien pasien) {
+        int konfirmasi = javax.swing.JOptionPane.showConfirmDialog(null,
+                "Konfirmasi pembayaran untuk " + pasien.nama + "?", "Konfirmasi",
+                javax.swing.JOptionPane.YES_NO_OPTION);
+        return konfirmasi == javax.swing.JOptionPane.YES_OPTION;
     }
 }
