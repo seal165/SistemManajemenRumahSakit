cf3bfe24e8c8f5d44be49e0b9ab7cbf6b79a178a
Eolia Shalbillah Gadis Suwandi <eoliasuwandi@gmail.com>
2025-04-18 14:40:16 +0700

    Update MetodePembayaran.java

--- Tugas 2/src/ManajemenRumahSakit/MetodePembayaran.java
+++ Tugas 2/src/ManajemenRumahSakit/MetodePembayaran.java
@@ -2,24 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ManajemenRumahSakit;
+package Bab2_ManajemenRumahSakit;
 
 /**
  *
  * @author Eolia Shalbillah
  */
-public enum MetodePembayaran { // Mendeklarasikan enum untuk metode pembayaran
-    TRANSFER_BANK, // Metode pembayaran melalui transfer bank
-    TUNAI, // Metode pembayaran secara tunai
-    KARTU_KREDIT; // Metode pembayaran menggunakan kartu kredit
+public class MetodePembayaran {
+    String metode;
 
-    @Override
-    public String toString() { // Override method toString untuk mengembalikan nama metode pembayaran yang lebih mudah dibaca
-        switch (this) { // Mengecek nilai enum saat ini
-            case TRANSFER_BANK: return "Transfer Bank"; // Mengembalikan string "Transfer Bank" jika enum adalah TRANSFER_BANK
-            case TUNAI: return "Tunai"; // Mengembalikan string "Tunai" jika enum adalah TUNAI
-            case KARTU_KREDIT: return "Kartu Kredit"; // Mengembalikan string "Kartu Kredit" jika enum adalah KARTU_KREDIT
-            default: return "Unknown"; // Mengembalikan "Unknown" jika tidak ada yang sesuai (seharusnya tidak terjadi)
-        }
+    public MetodePembayaran(String metode) {
+        this.metode = metode;
+    }
+
+    void dataMetode(String metode) {
+        this.metode = metode;
+    }
+
+    String cetakMetode() {
+        return metode;
     }
 }
