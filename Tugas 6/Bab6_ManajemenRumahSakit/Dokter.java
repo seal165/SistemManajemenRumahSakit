15b831b352d3901848c24f72e996ecfbec7510f9
Eolia Shalbillah Gadis Suwandi <eoliasuwandi@gmail.com>
2025-05-07 11:14:30 +0700

    Add files via upload

---
+++ Tugas 6/Bab6_ManajemenRumahSakit/Dokter.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bab6_ManajemenRumahSakit;
+
+import Bab5_ManajemenRumahSakit.*;
+
+/**
+ *
+ * @author Eolia Shalbillah
+ */
+public class Dokter { // Mendeklarasikan kelas Dokter
+    private String dokter; // Variabel untuk menyimpan nama dokter
+
+    // Konstruktor untuk menginisialisasi objek Dokter dengan nama dan spesialisasi
+    public Dokter(String dokter) {
+        this.dokter = dokter; // Menetapkan nilai parameter dokter ke variabel dokter
+    }
+
+    // Method getter untuk mendapatkan nama dokter
+    public String getDokter() { return dokter; }
+
+    // Override method toString untuk mengembalikan representasi string dari objek Dokter
+    @Override
+    public String toString() {
+        return "Dokter" + dokter;
+    }
+}
---
+++ Tugas 6/Bab6_ManajemenRumahSakit/Obat.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bab6_ManajemenRumahSakit;
+
+import Bab5_ManajemenRumahSakit.*;
+
+/**
+ *
+ * @author Eolia Shalbillah
+ */
+public class Obat {
+    protected String namaObat;
+    protected double harga;
+
+    public Obat(String namaObat, double harga) {
+        this.namaObat = namaObat;
+        this.harga = harga;
+    }
+
+    public String getNamaObat() { return namaObat; }
+    public double getHarga() { return harga; }
+
+    // Overloading method toString dengan parameter
+    public String toString(boolean includeHarga) {
+        if (includeHarga) {
+            return namaObat + " (Rp " + harga + ")";
+        } else {
+            return namaObat;
+        }
+    }
+}
