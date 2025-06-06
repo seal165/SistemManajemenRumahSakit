3bd69dd64a9fc47730590a27816dff68d4a9651c
Eolia Shalbillah Gadis Suwandi <eoliasuwandi@gmail.com>
2025-06-06 17:29:32 +0700

    Add files via upload

---
+++ Tugas 10/Bab10_ManajemenRumahSakit/BPJS.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Bab10_ManajemenRumahSakit;
+
+/**
+ *
+ * @author Eolia Shalbillah
+ */
+public interface BPJS {
+    double hitungTanggungan(double biaya);
+}
